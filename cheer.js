db479341b12f9467dca605839ac73f8ef540dc1c
Mehari David <david.mehari@gmail.com>
2022-04-11 20:02:13 +0200

    a4

--- cheer.js
+++ cheer.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 const cheers = param => console.log(`Cheer for the ${parameter}`);
-=======
+
 const cheer = name => console.log(name);
 
 cheer("javascript");
->>>>>>> f2d6b64207d7b1e28f3e5d852b029ad6f7d35109
--- count.js
+++ count.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+
 function count (param) {
   console.log(26 + param
   )
-=======
+
 function count () {
   console.log("1")
->>>>>>> be456c9d47339bebdce659cd3d6830f83a15bb8c
+
 }
 
 
