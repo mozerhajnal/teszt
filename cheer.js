50474b11be60ae778fe2f8f5c3d0c6c38e614e5c
Hazsni <mozerhajni@gmail.com>
2022-04-11 20:19:58 +0200

    c5

--- cheer.js
+++ cheer.js
@@ -1,8 +1,10 @@
 
 
-function cheers (parameter) {
+function cheers (parameter,param2) {
   console.log(`Cheer for the ${parameter}`)
+  console.log(`Cheer for the ${param2}`)
 }
 
 cheers("javascript")
+cheers("github")
 
