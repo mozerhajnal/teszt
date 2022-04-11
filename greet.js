f188b847c412ab20ff029e3465b673a4f7bfb3bb
Hazsni <mozerhajni@gmail.com>
2022-04-11 20:04:18 +0200

    c4

--- greet.js
+++ greet.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 function greet() {
-  console.log("Greetings");
-=======
+  console.log("Greetings");}
+  
+
 function greet(name) {
   console.log(name);
->>>>>>> f2d6b64207d7b1e28f3e5d852b029ad6f7d35109
 }
